701d9ca8f9e82c7709b739ef052491570ecb22ef
Fixated84 <nolanryan@bigpond.com>
2016-04-03 19:16:46 +1000

    start of assessment

--- README.md
+++
@@ -1,2 +0,0 @@
-# assessment1_sem2_nolan
-ICTDBS504: Integrate database with website
---
+++ login.php
@@ -0,0 +1,247 @@
+<?php
+$message = "<p></p>";
+
+if($_POST) {
+
+$username = test_input($_POST['username']);
+$password = md5($_POST['password']);
+
+
+include("connect.php");
+
+$query =  "SELECT * FROM `Salespersons` WHERE `Username` = '$username' AND `Password` = '$password'";
+
+$result = mysqli_query($con, $query);
+if (mysqli_num_rows($result)==1) {
+ //  echo "<br> username: " . $username . " is in the database";
+	session_start();
+	$_SESSION['username']= 'true';
+header('location:employee.php');
+
+}else {
+ $message = "<br><p>Incorrect Username or Password</p>";
+
+}
+ 
+   }
+   
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+ 
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="../images/icon.ico">
+
+ 
+	<script src="../js/jquery-1.11.2.min.js"></script>
+    
+ 
+
+    <title>ICTDBS504: Integrate database with website</title>
+    
+ 
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/bootstrap.css" rel="stylesheet">	
+  </head>
+  <body>
+  <br><br>
+   <nav class="navbar navbar-inverse navbar-fixed-top">
+     <!--Testing Fix Nav with navbar-fixed-top-->
+     
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button  type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#inverseNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+       <!-- <a class="navbar-brand" href="#">AllStyle Homes</a>-->
+        
+        </div>
+         
+              <div class="navbar-header1">
+        <button   type="button" class="navbar-toggle collapsed menu-toggle pull-left" data-toggle="collapse" ><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+ 
+         <!-- <a class="navbar-brand" href="#">AllStyle Homes</a>-->
+        
+        </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="inverseNavbar1">
+        <ul class="nav navbar-nav">
+       <!--   <li class="active"><a href="#">Link<span class="sr-only">(current)</span></a></li>-->
+           <li ><a href="../index.html">Home</a></li>
+           <li class="active"><a href="employee.php">Employee Login</a></li>
+          <!-- <li><a href="pages/design.html">Design</a></li>-->
+  
+           <li><a href="../pages/ourprocess.html">Privacy Policy</a></li>
+                             
+        </ul>
+     
+        
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
+  
+  <div class="container">
+   
+    <div class="row">
+        
+        <div class="col-sm-7 col-sm-offset-1 col-lg-offset-1 col-lg-10">
+         <hr>
+       <h3 >ICTDBS504: Integrate database with website</h3>  <br>
+
+     
+     <?php
+echo $message;
+ ?>
+
+ 
+<br>
+<form class="loginform" method="POST">
+  <fieldset class="account-info">
+    <label>
+      Username
+      <input type="text" name="username">
+    </label>
+    <label>
+      Password
+      <input type="password" name="password">
+    </label>
+  </fieldset>
+  <fieldset class="account-action formcentre">
+    <input class="btn" type="submit" name="submit" value="Login">
+     
+  </fieldset>
+  
+</form>
+   <br>
+   <br>
+       
+</div>
+      
+        </div>
+        
+ 
+        
+     
+      
+      <hr>
+        <div class="row center-block">
+    <div class="text-center col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-0 hello">
+      
+      <p>Copyright &copy; 2016 &middot; All Rights Reserved</p>
+    </div>
+      
+  </div>
+    <hr>
+    <!--End Row-->   
+
+<!--End Wrapper-->
+  </div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="../js/jquery-1.11.2.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="../js/bootstrap.js"></script>
+    <script type="text/javascript">
+$(document).ready(function() {
+	
+	var menu = "close";
+	
+   $('.menu-toggle').click(function()  {
+	   
+	   if (menu == "close") {
+		   	   
+	   $('.navbarside').css('-webkit-transform', 'translate(0,0)');
+	   $('.navbarside').css('-moz-transform', 'translate(0,0)'); 
+	   $('.navbarside').css('-o-transform', 'translate(0,0)'); 
+	   $('.navbarside').css('transform', 'translate(0,0)'); 
+ 
+	   $('.container').css('-webkit-transform', 'translate(30%,0)');
+	   $('.container').css('-moz-transform', 'translate(30%,0)');
+	   $('.container').css('-o-transform', 'translate(30%,0)');	   
+	   $('.container').css('transform', 'translate(30%,0)');
+		
+	   $('.navbar-header1').css('-webkit-transform', 'translate(0,0)');
+	   $('.navbar-header1').css('-moz-transform', 'translate(0,0)');	   
+	   $('.navbar-header1').css('-o-transform', 'translate(0,0)');	   
+	   $('.navbar-header1').css('transform', 'translate(0,0)');	   
+	   
+	   $('.navbar-header1').css('-webkit-transform', 'translate(30%,0)');
+	   $('.navbar-header1').css('-moz-transform', 'translate(30%,0)');   
+	   $('.navbar-header1').css('-o-transform', 'translate(30%,0)');
+	   $('.navbar-header1').css('transform', 'translate(30%,0)');   
+	   
+	   menu = "open";
+	   
+	   }else {
+		   
+	   $('.navbarside').css('-webkit-transform', 'translate(-100%,0)');
+	   $('.navbarside').css('-moz-transform', 'translate(-100%,0)');	   
+	   $('.navbarside').css('-o-transform', 'translate(-100%,0)');	   
+	   $('.navbarside').css('transform', 'translate(-100%,0)');	   
+	   
+	   $('.container').css('-webkit-transform', 'translate(0,0)');
+	   $('.container').css('-moz-transform', 'translate(0,0)');	   
+	   $('.container').css('-o-transform', 'translate(0,0)');
+	   $('.container').css('transform', 'translate(0,0)');	   
+	   	   
+	   $('.navbar-header1').css('-webkit-transform', 'translate(-100%,0)');
+	   $('.navbar-header1').css('-moz-transform', 'translate(-100%,0)');	   
+	   $('.navbar-header1').css('-o-transform', 'translate(-100%,0)');	   
+	   $('.navbar-header1').css('transform', 'translate(-100%,0)');	   
+	   
+	   $('.navbar-header1').css('-webkit-transform', 'translate(0,0)');
+	   $('.navbar-header1').css('-moz-transform', 'translate(0,0)');
+	   $('.navbar-header1').css('-o-transform', 'translate(0,0)');	   
+	   $('.navbar-header1').css('transform', 'translate(0,0)');
+	   
+	  
+	   
+	  menu = "close";
+	  
+	  
+	  
+	   }
+	});   
+});
+</script>
+ <!--    <script type="text/javascript">
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+  
+ document.getElementsByClassName("rickroll").item("iframe").src = "http://www.youtube.com/embed/dQw4w9WgXcQ?html5=1&autoplay=1";
+
+  
+})
+</script> -->
+
+<script type="text/javascript">
+$('#myModal').on('shown.bs.modal', function () {
+      
+        $('#myModal').modal('show');
+       
+   document.getElementsByClassName("rickroll").item("iframe").src = "https://www.youtube.com/embed/dQw4w9WgXcQ?html5=1&autoplay=1";
+    });
+
+    $('#myModal').click(function () {
+      document.getElementsByClassName("rickroll").item("iframe").src = "1";
+ 
+  
+    });
+</script>
+ 
+
+
+</body>
+</html>
